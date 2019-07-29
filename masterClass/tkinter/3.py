d4b7981ce1e14bcf77f2005e385216921a369c9e
ma <ma_yamaki@yahoo.com>
2019-07-29 19:46:13 +0900

    momon

---
+++ masterClass/tkinter/3.py
@@ -0,0 +1,32 @@
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+
+
+mainWindow = tkinter.Tk()
+
+
+mainWindow.title("Hello world")
+mainWindow.geometry('640x480+8+400')
+
+label = tkinter.Label(mainWindow, text="Hellow world")
+label.pack(side='top')
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+
+canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
+#canvas.pack(side='left', fill=tkinter.X, expand=True)
+canvas.pack(side='left', fill=tkinter.Y)
+#canvas.pack(side='top', fill=tkinter.BOTH, expand=True)
+
+button1 = tkinter.Button(mainWindow, text='Button1')
+button2 = tkinter.Button(mainWindow, text='Button2')
+button3 = tkinter.Button(mainWindow, text='Button3')
+button1.pack(side='left', anchor='n')
+button2.pack(side='left', anchor='s')
+button3.pack(side='left')
+
+mainWindow.mainloop()
---
+++ masterClass/tu_first.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+color('red','yellow')
+
+forward(399)
+right(30)
+forward(333)
+circle(59)
+
+done()
