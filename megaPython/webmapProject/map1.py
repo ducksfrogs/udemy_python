721269116cb8c7983e6c70338bfa677a79305132
ma <ma_yamaki@yahoo.com>
2019-09-25 18:29:58 +0900

    deleted testing version

--- megaPython/webmapProject/map1.py
+++ megaPython/webmapProject/map1.py
@@ -11,7 +11,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain" 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el), parse_html=True), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el), parse_html=True),
+                               icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save("Map1.html")
