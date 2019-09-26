efb5c5580f8b7fd18afe1e699adec978439b80d8
ma <ma_yamaki@yahoo.com>
2019-09-26 09:12:48 +0900

    momo

--- numerical_methods_with_python/codes/interpolation/curve_fitting.py
+++ numerical_methods_with_python/codes/interpolation/curve_fitting.py
@@ -1,4 +1,28 @@
-x = [0, 20, 40, 60, 80, 100]
-y = [26.0, 48.6, 61.6, 71.2, 74.8, 75.2]
+import numpy as np
 
+x = np.arange(6)
+y = np.array([2, 8, 14, 28, 39, 62])
 
+m = len(x)
+n = 2
+
+A = np.zeros((n+1, n+1))
+B = np.zeros(n+1)
+a = np.zeros(n+1)
+
+for row in range(n+1):
+    for col in range(n+1):
+        if row == 0 and col == 0:
+            A[row, col] = m
+            continue
+        A[row, col] = np.sum(x**(row+col))
+
+    B[row] = np.sum(x**row * y)
+
+a = np.linalg.solve(A, B)
+
+print('The polynomial :')
+print('f(x) = \t %f \t' %a[0])
+
+for i in range(1, n+1):
+    print('\t %+f x^%d' % (a[i], i))
